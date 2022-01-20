6a5da94b4cb8b5f8260297f56f05b9f37935c28e
Madison Hagio <madisonhagio@gmail.com>
2022-01-19 16:44:28 -1000

    Initial Commit

---
+++ Hackwich1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackwich1
+//
+//  Created by madison hagio on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
